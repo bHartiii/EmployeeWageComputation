c0e1e49b1a9183fc187d422cdbe46c19ed2666f4
bharti mali <malibharti5@gmail.com>
2020-10-28 17:13:59 +0530

    ADD : Employee Attendance Function

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,9 +1,16 @@
-
-public class EmployeeWageComputation {
-
-	public static void main(String[] args) {
-		System.out.println("WWELCOME");
-
+import java.util.*;
+public class EmployeeWageComputation
+{	static int isPresent;
+	public int employeeAttendance()
+	{
+		Random random=new Random();
+	        int isPresent=random.nextInt(2);
+	        return isPresent;
 	}
-
+	public static void main(String args[])
+    	{
+		EmployeeWageComputation empWage=new EmployeeWageComputation();
+		isPresent=empWage.employeeAttendance();
+		System.out.println(isPresent);
+   	}
 }
