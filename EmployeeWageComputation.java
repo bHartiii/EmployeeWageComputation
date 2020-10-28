78a4718ff41dcbc36d311db8b22656bbdd3b4109
bharti mali <malibharti5@gmail.com>
2020-10-28 18:43:57 +0530

    ADD : Part time emplyoee's wage calculation

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,8 +1,10 @@
 import java.util.*;
 public class EmployeeWageComputation
-{	static String attendance="";
+{	static String attendance;
+	static String isPartTime;
 	static final int wagePerHour=20;
 	static final int fullDayHour=8;
+	static final int partTimeHour=4;
 	static float dailyWage;
 	public String employeeAttendance()
 	{
@@ -16,17 +18,35 @@ public class EmployeeWageComputation
 		}
 	        return attendance;
 	}
+	public String isPartTime(){
+		Random random=new Random();
+		int isPartTime=random.nextInt(2);
+		if(isPartTime==0){
+			EmployeeWageComputation.isPartTime="Yes";
+		}
+		else{
+			EmployeeWageComputation.isPartTime="No";
+		}
+		return EmployeeWageComputation.isPartTime;
+	}
 	public float dailyWageCalculation(){
 		float dailyWage=0;
 		if(attendance.equals("present")){
-			dailyWage=wagePerHour*fullDayHour;
+			if(isPartTime.equals("Yes")){
+				dailyWage=wagePerHour*partTimeHour;
+			}
+			else{
+				dailyWage=wagePerHour*fullDayHour;
+			}
 		}
 		return dailyWage;
 	}
+
 	public static void main(String args[])
     	{
 		EmployeeWageComputation empWage=new EmployeeWageComputation();
 		empWage.employeeAttendance();
+		empWage.isPartTime();
 		dailyWage=empWage.dailyWageCalculation();
    		System.out.println("Daily Wage - "+dailyWage );
 	}
