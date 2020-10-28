d72135cd8033630ab238203a60b86066e569da6a
bharti mali <malibharti5@gmail.com>
2020-10-28 18:06:38 +0530

    ADD : Daily wage calculation method

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,16 +1,33 @@
 import java.util.*;
 public class EmployeeWageComputation
-{	static int isPresent;
-	public int employeeAttendance()
+{	static String attendance="";
+	static final int wagePerHour=20;
+	static final int fullDayHour=8;
+	static float dailyWage;
+	public String employeeAttendance()
 	{
 		Random random=new Random();
 	        int isPresent=random.nextInt(2);
-	        return isPresent;
+		if(isPresent==0){
+			EmployeeWageComputation.attendance="absent";
+		}
+		else{
+			EmployeeWageComputation.attendance="present";
+		}
+	        return attendance;
+	}
+	public float dailyWageCalculation(){
+		float dailyWage=0;
+		if(attendance.equals("present")){
+			dailyWage=wagePerHour*fullDayHour;
+		}
+		return dailyWage;
 	}
 	public static void main(String args[])
     	{
 		EmployeeWageComputation empWage=new EmployeeWageComputation();
-		isPresent=empWage.employeeAttendance();
-		System.out.println(isPresent);
-   	}
+		empWage.employeeAttendance();
+		dailyWage=empWage.dailyWageCalculation();
+   		System.out.println("Daily Wage - "+dailyWage );
+	}
 }
