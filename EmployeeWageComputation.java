610ea176d9c9911598926e977f5d43f5c524f51e
bharti mali <malibharti5@gmail.com>
2020-11-03 00:56:38 +0530

    ADD : BuilderClass added for different companies

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,33 +2,16 @@ import java.util.*;
 public class EmployeeWageComputation
 {       String attendance;
         String isPartTime;
-        String companyName;
+	String empName;
         float dailyWage=0;
         float monthlyWage=0;
         int totalWorkingDays=0;
         int totalWorkingHours=0;
         int workingHours=0;
-<<<<<<< HEAD
 
 	public EmployeeWageComputation(String empName){
 		this.empName=empName;
 	}
-=======
-        int WAGE_PER_HOURS;
-        int FULL_DAY_HOURS;
-        int PART_TIME_HOURS;
-        int MAX_WORKING_HOURS;
-        int MAX_WORKING_DAYS;
-
-        public EmployeeWageComputation(String companyName,int wage,int fullDayHours,int partTimeHours,int maxWorkingHours,int maxWorkingDays){
-                this.companyName=companyName;
-                this.WAGE_PER_HOURS=wage;
-                this.FULL_DAY_HOURS=fullDayHours;
-                this.PART_TIME_HOURS=partTimeHours;
-                this.MAX_WORKING_HOURS=maxWorkingHours;
-                this.MAX_WORKING_DAYS=maxWorkingDays;
-        }
->>>>>>> master
 
         public String employeeAttendance()
         {
@@ -44,22 +27,22 @@ public class EmployeeWageComputation
                 }
                 return attendance;
         }
-         public int getWorkingHours(){
+         public int getWorkingHours(int partTimeHours,int fullDayHours){
                 Random random=new Random();
                 int isPartTime=random.nextInt(2);
                 switch(isPartTime){
                         case 0:
-                                workingHours=PART_TIME_HOURS;
+                                workingHours=partTimeHours;
                                 break;
                         case 1:
-                                workingHours=FULL_DAY_HOURS;
+                                workingHours=fullDayHours;
                                 break;
                 }
                 return workingHours;
         }
-        public float dailyWageCalculation(){
+        public float dailyWageCalculation(int wagePerHour){
                 if(attendance.equals("present")){
-                     dailyWage=WAGE_PER_HOURS*workingHours;
+                     dailyWage=wagePerHour*workingHours;
                      totalWorkingHours=totalWorkingHours+workingHours;
                 }
                 else{
@@ -67,26 +50,18 @@ public class EmployeeWageComputation
                 }
                 return dailyWage;
         }
-        public void monthlyWageCalculation(){
-                while(totalWorkingDays!=MAX_WORKING_DAYS && totalWorkingHours!=MAX_WORKING_HOURS){
+        public void monthlyWageCalculation(CompanyBuilder company){
+                while(totalWorkingDays!=company.maxWorkingDays && totalWorkingHours!=company.maxWorkingHours){
                         employeeAttendance();
-                        getWorkingHours();
-                        dailyWage=dailyWageCalculation();
+                        getWorkingHours(company.partTimeHours,company.fullDayHours);
+                        dailyWage=dailyWageCalculation(company.wagePerHour);
                         monthlyWage=monthlyWage+dailyWage;
                         totalWorkingDays++;
                 }
-                System.out.println("Company Name - "+companyName);
+                System.out.println("Company Name - "+company.companyName);
                 System.out.println("Total Working Days - "+totalWorkingDays);
                 System.out.println("Total Working Hours - "+totalWorkingHours);
                 System.out.println("Monthly Wage - "+monthlyWage );
 
         }
-<<<<<<< HEAD
-=======
-        public static void main(String args[])
-        {
-                EmployeeWageComputation dMart=new EmployeeWageComputation("DMart",30,16,8,320,20);
-                dMart.monthlyWageCalculation();
-        }
->>>>>>> master
 }
